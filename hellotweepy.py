9e8965eef9f677bda9cc981aa48ba4f31ea83293
ubuntu <Mandar Mahajan>
2017-02-08 17:48:11 +0530

    Testing how git bash commands work

--- hellotweepy.py
+++ hellotweepy.py
@@ -2,14 +2,14 @@ import tweepy
 import base64
 
 
-def paginate(iterable, page_size):
+"""def paginate(iterable, page_size):
     while True:
         i1, i2 = itertools.tee(iterable)
         iterable, page = (itertools.islice(i1, page_size, None),
                 list(itertools.islice(i2, page_size)))
         if len(page) == 0:
             break
-        yield page
+        yield page"""
 
 consumer_key="cojo660B8bkiJSpOpmzlDaHpP"
 
